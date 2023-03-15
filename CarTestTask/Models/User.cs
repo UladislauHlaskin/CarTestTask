1092d0dc25076ccdf93e393d344a1c5a81e9b208
UladislauHlaskin <Uladislau.Hlaskin@gmail.com>
2023-03-15 10:24:08 +0300

    Added DB user auth

--- CarTestTask/Models/User.cs
+++ CarTestTask/Models/User.cs
@@ -12,22 +12,5 @@ namespace CarTestTask.Models
         [Required]
         [DefaultValue("12345")]
         public string Password { get; set; }
-
-        //#region DB substitute
-        //private static System.Collections.Generic.List<User> Users;
-
-        //public static System.Collections.Generic.List<User> GetUsers()
-        //{
-        //    if (Users == null)
-        //    {
-        //        Users = new System.Collections.Generic.List<User>()
-        //        {
-        //            new User() { UserName = "Admin", Password = "12345"},
-        //            new User() { UserName = "User", Password = "123456"},
-        //        };
-        //    }
-        //    return Users;
-        //}
-        //#endregion
     }
 }
